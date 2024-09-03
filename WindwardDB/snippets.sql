9fcf25ecf9a43e1d4d4cc0a7cd123bad2d7f2d7e
Amneris Calle <amneris.calle@gmail.com>
2024-09-02 23:02:37 -0300

    correccion snippets

--- WindwardDB/snippets.sql
+++ WindwardDB/snippets.sql
@@ -3,21 +3,27 @@
 SET @cliente = 2;
 SELECT * FROM productos_con_precios WHERE lista = fn_generar_variable_lista(@cliente);
 
--- Hacer un pedido. El primer argumento del procedure es el id del cliente y el segundo es el json producto-cantidad
+-- Hacer un pedido. El primer argumento del procedure es el id del cliente y el segundo es el json producto-cantidad. Conviene primero ver la tabla pedidos para despues poder comparar.
 
 CALL sp_generar_pedidos (3,'[{"producto":1,"cantidad":2},{"producto":2,"cantidad":10},{"producto":4,"cantidad":3}]');
+
+-- Ver el pedido recien creado 
+
 SET @cliente = 3;
 SELECT * FROM pedidos_detallados WHERE id_cliente = @cliente;
 
 -- Hacer un pedido en que la cantidad de uno de los productos es mayor que el stock
 
 CALL sp_generar_pedidos (2,'[{"producto":1,"cantidad":50},{"producto":5,"cantidad":1}]');
+
+-- Ver el pedido recien generado 
+
 SET @cliente = 2;
 SELECT * FROM pedidos_detallados WHERE id_cliente = @cliente;
 
 -- Ver pedidos con el detalle de la orden de compra de todos los clientes
 
-SELECT * FROM pedidos_detallados
+SELECT * FROM pedidos_detallados;
 
 -- Ver pedidos con el detalle de la orden de compra de un cliente en particular
 
@@ -32,6 +38,6 @@ SELECT * FROM pivot_productos_con_precios;
 
 SELECT * FROM totales_por_fecha;
 
--- Aprobar un pedido (y dar de baja del stock los productos involucrados)
+-- Aprobar un pedido (y dar de baja del stock los productos involucrados). En este caso conviene primero ver la tabla productos para ver el stock de los mismos y comparar el stock despues de correr el procedimiento para aprobar pedido
 
-CALL sp_aprobar_pedido (5,2)
+CALL sp_aprobar_pedido (5,2);
